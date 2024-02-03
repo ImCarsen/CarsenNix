51147c02117d624230b6858b08f008cca1e8934e
ImCarsen <carsen@imcarsen.com>
2024-02-02 19:39:11 -0600

    oops

--- configuration.nix
+++ configuration.nix
@@ -40,11 +40,10 @@
     nixpkgs.config.allowUnfree = true;
 
     # Packages
-    { inputs, pkgs, ... }: {
-        programs.hyprland = {
+
+    programs.hyprland = {
         enable = true;
         package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-        };
     };
 
     environment.systemPackages = with pkgs; [
